f37247473720450081f2d8ce753ca657f774d868
sickmydig <lamtrongphan@gmail.com>
2021-03-09 09:50:10 +0700

    fix on main dev

--- push-notification/src/main/java/com/test/pushnotification/model/Notification.java
+++ push-notification/src/main/java/com/test/pushnotification/model/Notification.java
@@ -3,9 +3,11 @@ package com.test.pushnotification.model;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
 public class Notification {
     private String from;
     private String message;
